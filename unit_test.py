1c1cc0aa24e51c4985e1c96b7e234ec9862324c8
Cantthinkofabetterusername <pp3drolinar3s@gmail.com>
2026-04-29 10:38:03 -0600

    code

---
+++ unit_test.py
@@ -0,0 +1,12 @@
+from searching import *
+
+case_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+case_2 = list(range(32))
+case_3 = list(range(128))
+
+busqueda_lineal(case_1, 8)
+busqueda_binaria(case_1, 8)
+busqueda_lineal(case_1, 16)
+busqueda_binaria(case_1, 16)
+busqueda_lineal(case_1, 67)
+busqueda_binaria(case_1, 67)
